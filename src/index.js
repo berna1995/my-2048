8c27eb1e7a2ca2be06dcb132cf1ce254c5fd4959
Matteo Bernabito <matteo@bernabito.it>
2021-07-26 00:38:18 +0200

    Add some base styling and base structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-
-class Game extends React.Component {
+class Box extends React.Component {
     render() {
         return (
-            <div className="gamepanel">
-                Game content here
+            <div>Box</div>
+        );
+    }
+}
+
+class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    generateBoxes(rows, columns) {
+        let boxes = [];
+        for(let i=0; i<rows; i++) {
+            for(let j=0; j<columns; j++) {
+                boxes.push(<div className="block">Box {i} {j}</div>)
+            }
+        }
+        return boxes;
+    }
+
+    render() {
+        return (
+            <div className="gamegrid">
+                {this.generateBoxes(this.props.rows,this.props.columns)}
             </div>
         );
     }
@@ -15,6 +38,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <Game rows={4} columns={4} />,
     document.getElementById('root')
 );
