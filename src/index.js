27c37d8e779b952d2e4f3e4b1fb37c121063dae3
Matteo Bernabito <matteo@bernabito.it>
2021-07-27 00:13:45 +0200

    Refactor a bit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class BackgroundBox extends React.Component {
     render() {
         return (
-            <div className="bg-block" style={{ gridRow: this.props.row + 1, gridColumn: this.props.col + 1 }} />
+            <div className="bg-block"/>
         );
     }
 }
@@ -21,10 +21,8 @@ class ValueBox extends React.Component {
 }
 
 class Board extends React.Component {
-
     constructor(props) {
         super(props);
-
     }
 
     generateBackgroundBlocks(rows, columns) {
@@ -32,9 +30,39 @@ class Board extends React.Component {
 
         for (let i = 0; i < rows; i++)
             for (let j = 0; j < columns; j++)
-                boxes.push(<BackgroundBox row={i} col={j} />);
+                boxes.push(<BackgroundBox />);
 
         return boxes;
+    }
+
+    handleKeyDown(keyevent) {
+        switch(keyevent.keyCode) {
+            case 40: 
+                // Arrow down
+                console.log("Arrow down pressed");
+                break;
+            case 39:
+                // Arrow right
+                console.log("Arrow right pressed");
+                break;
+            case 38:
+                // Arrow up
+                console.log("Arrow up pressed");
+                break;
+            case 37:
+                // Arrow left
+                console.log("Arrow left pressed");
+                break;
+            default: return;
+        }
+    }
+
+    componentDidMount() {
+        document.onkeydown = this.handleKeyDown;
+    }
+
+    componentWillUnmount() {
+        document.onkeydown = null;
     }
 
     render() {
@@ -58,7 +86,6 @@ class Header extends React.Component {
 }
 
 class Game extends React.Component {
-
     render() {
         return (
             <div className="gamecontainer">
